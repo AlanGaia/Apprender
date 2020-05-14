6bfba2016705cc5fb5dc32051f06686ae86991c8
valiantsuli <alan.j.gaia@gmail.com>
2020-05-14 04:57:11 -0300

    reddit card teach

--- frontend/js/ajax.js
+++ frontend/js/ajax.js
@@ -9,11 +9,32 @@ function registerForm() {
       res.innerHTML = "";
 
       res.innerHTML += `
+      <!-- Modal Trigger -->
+        <a class="waves-effect waves-light btn modal-trigger"  href="#modal1"
+          >Modal</a
+        >
+
+        <!-- Modal Structure -->
+        <div id="modal1" class="modal">
+          <div class="modal-content">
+            <img src="images/adr.jpg" class="avatar" alt="" />
+          </div>
+        </div>
+
+
       `;
     } else {
       let res = document.getElementById("app");
       res.innerHTML = `Hubo un error ${this.status}`;
     }
+  });
+
+  const app = document.getElementById("app");
+  app.addEventListener("click", function () {
+    console.log("si señor");
+
+    var elems = document.querySelectorAll(".modal");
+    var instances = M.Modal.init(elems, {});
   });
 
   xhttp.open("GET", "../../backend/registerForm.json");
